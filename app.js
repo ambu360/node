10c21d76969bb4d4176ed0418df94e1445cfc5d7
ambu360 <75344498+ambu360@users.noreply.github.com>
2022-07-12 14:57:56 -0400

    event loop example

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-console.log('first tasl');
-setTimeout(()=>{
-    console.log('second tast');
-},0)
-console.log('next task');
+const {readFile} = require('fs')
+
+console.log('started first task')
+
+readFile('./content/first.txt','utf8',(err,result)  =>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log(result)
+    console.log('completed first task')
+})
+console.log('starting next tast')
